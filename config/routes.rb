66f8fe3d15bc40225a5646b0befe4aca068ede39
MSI\Leo <engrleonelfelezario812@gmail.com>
2024-01-12 02:45:13 +0800

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "blogs#index"
+  resources :blogs, only: [:show] do 
+    resources :feedbacks, only: [:create]
+  end
 end
