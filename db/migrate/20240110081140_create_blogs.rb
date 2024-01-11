22a8701efdde0507865c78a854fde5c6b29c7e21
MSI\Leo <engrleonelfelezario812@gmail.com>
2024-01-12 02:42:04 +0800

    create feedbacks blogs and users table

--- db/migrate/20240110081140_create_blogs.rb
+++ db/migrate/20240110081140_create_blogs.rb
@@ -3,6 +3,8 @@ class CreateBlogs < ActiveRecord::Migration[7.1]
     create_table :blogs do |t|
       t.string :title
       t.text :body
+      t.integer :feedbacks_count, default: 0
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_10_081140) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_11_141413) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "feedbacks_count", default: 0
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.text "content"
+    t.integer "blog_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_feedbacks_on_blog_id"
+    t.index ["user_id"], name: "index_feedbacks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,7 +41,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_081140) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
+  add_foreign_key "feedbacks", "blogs"
+  add_foreign_key "feedbacks", "users"
 end
