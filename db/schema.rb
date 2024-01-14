05555ae2f64afd478b0eba8f26a30f1e1e743277
MSI\Leo <engrleonelfelezario812@gmail.com>
2024-01-14 16:24:48 +0800

    rails db:system:change --to=postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_11_141413) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "feedbacks_count", default: 0
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
@@ -23,8 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_141413) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.text "content"
-    t.integer "blog_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "blog_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["blog_id"], name: "index_feedbacks_on_blog_id"
